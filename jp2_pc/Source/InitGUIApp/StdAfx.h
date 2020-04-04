46b8caff731cbde3054bbb022fc5652f760f7e3e
Michael <michael_kuerbis@web.de>
2020-04-04 13:44:57 +0200

    InitGUIApp: enable usage of old Direct3D headers

--- jp2_pc/Source/InitGUIApp/StdAfx.h
+++ jp2_pc/Source/InitGUIApp/StdAfx.h
@@ -5,6 +5,10 @@
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
+#undef DIRECT3D_VERSION //Defined somewhere in the MFC headers for Direct3D 9+
+#define DIRECT3D_VERSION 0x0700 //We must set the correct version here to use old Direct3D
+
+
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
 #ifndef _AFX_NO_AFXCMN_SUPPORT
