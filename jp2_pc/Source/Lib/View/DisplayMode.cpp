e16777209eccc3a0ddc3f2b9451dfa0c75824bb4
Michael <michael_kuerbis@web.de>
2020-05-26 00:06:06 +0200

    DisplayMode: add WindowMode enum and configuration query

---
+++ jp2_pc/Source/Lib/View/DisplayMode.cpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "DisplayMode.hpp"
+#include "Lib/Sys/RegInit.hpp"
+#include "Lib/Sys/reg.h"
+
+WindowMode GetWindowModeConfigured()
+{
+	int selection = GetRegValue("WindowMode", 0);
+	if (selection > static_cast<int>(WindowMode::EXCLUSIVE))
+		selection = 0;
+	return static_cast<WindowMode>(selection);
+}
+
+WindowMode GetWindowModeActual()
+{
+	if (GetRegValue(strFLAG_D3D, DEFAULT_D3D))
+		return WindowMode::EXCLUSIVE;
+	else
+		return GetWindowModeConfigured();
+}
