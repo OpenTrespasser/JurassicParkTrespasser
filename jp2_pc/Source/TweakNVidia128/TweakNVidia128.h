319878846e74b1efcba619c6ca487c7c61ea65ce
Michael <michael_kuerbis@web.de>
2020-11-15 21:46:26 +0100

    TweakNVidia128.h: adjust include directives for case sensitivity

--- jp2_pc/Source/TweakNVidia128/TweakNVidia128.h
+++ jp2_pc/Source/TweakNVidia128/TweakNVidia128.h
@@ -12,7 +12,7 @@
 	#error include 'stdafx.h' before including this file for PCH
 #endif
 
-#include "resource.h"		// main symbols
+#include "Resource.h"		// main symbols
 
 /////////////////////////////////////////////////////////////////////////////
 // CTweakNVidia128App:
