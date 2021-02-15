6180d618e81256ed4eda46e320d252c94f3edd4e
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:22 +0100

    DialogTerrain.hpp: use override keyword

--- jp2_pc/Source/GUIApp/DialogTerrain.hpp
+++ jp2_pc/Source/GUIApp/DialogTerrain.hpp
@@ -94,7 +94,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogTerrain)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
