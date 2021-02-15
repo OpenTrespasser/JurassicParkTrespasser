65089c672bdc596542efc2fb61d1904324c6ed97
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:16 +0100

    DialogOcclusion.hpp: use override keyword

--- jp2_pc/Source/GUIApp/DialogOcclusion.hpp
+++ jp2_pc/Source/GUIApp/DialogOcclusion.hpp
@@ -45,7 +45,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogOcclusion)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
