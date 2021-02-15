06451f30f975109fb4187c641c27698b6a5d5d9f
Michael <michael_kuerbis@web.de>
2021-02-15 21:41:21 +0100

    TweakNVidia128Dlg.cpp: use override keyword

--- jp2_pc/Source/TweakNVidia128/TweakNVidia128Dlg.cpp
+++ jp2_pc/Source/TweakNVidia128/TweakNVidia128Dlg.cpp
@@ -49,7 +49,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CAboutDlg)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
