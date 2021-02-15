1ab3d328684db9eeeb52de9be79712526acd3edb
Michael <michael_kuerbis@web.de>
2021-02-15 21:41:10 +0100

    AudioTest.h: use override keyword

--- jp2_pc/Source/Test/AudioTest.h
+++ jp2_pc/Source/Test/AudioTest.h
@@ -21,7 +21,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CAudioTestApp)
 	public:
-	virtual BOOL InitInstance();
+	virtual BOOL InitInstance() override;
 	//}}AFX_VIRTUAL
 
 // Implementation
