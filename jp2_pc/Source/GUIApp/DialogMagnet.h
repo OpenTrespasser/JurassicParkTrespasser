1a6a242b56a24e49f0331793249ad76ae80b6317
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:14 +0100

    DialogMagnet.h: use override keyword

--- jp2_pc/Source/GUIApp/DialogMagnet.h
+++ jp2_pc/Source/GUIApp/DialogMagnet.h
@@ -50,8 +50,8 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogMagnet)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
+	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam) override;
 	//}}AFX_VIRTUAL
 
 // Implementation
