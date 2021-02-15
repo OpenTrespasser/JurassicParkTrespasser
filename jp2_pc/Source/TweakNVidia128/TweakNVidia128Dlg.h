c1292011c66d5b23526d8d15d3c22363bac83b99
Michael <michael_kuerbis@web.de>
2021-02-15 21:41:21 +0100

    TweakNVidia128Dlg.h: use override keyword

--- jp2_pc/Source/TweakNVidia128/TweakNVidia128Dlg.h
+++ jp2_pc/Source/TweakNVidia128/TweakNVidia128Dlg.h
@@ -26,7 +26,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CTweakNVidia128Dlg)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;	// DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
@@ -35,7 +35,7 @@ protected:
 
 	// Generated message map functions
 	//{{AFX_MSG(CTweakNVidia128Dlg)
-	virtual BOOL OnInitDialog();
+	virtual BOOL OnInitDialog() override;
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
