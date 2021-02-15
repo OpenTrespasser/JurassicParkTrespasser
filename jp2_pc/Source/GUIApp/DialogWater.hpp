be720ebc5d40ef1f6058d5ea35acc7377546be23
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:24 +0100

    DialogWater.hpp: use override keyword

--- jp2_pc/Source/GUIApp/DialogWater.hpp
+++ jp2_pc/Source/GUIApp/DialogWater.hpp
@@ -52,7 +52,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogWater)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
