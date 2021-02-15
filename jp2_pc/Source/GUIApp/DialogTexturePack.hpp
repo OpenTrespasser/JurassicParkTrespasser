28fb7d2fb9f6338265744ec7228325cd8541d048
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:22 +0100

    DialogTexturePack.hpp: use override keyword

--- jp2_pc/Source/GUIApp/DialogTexturePack.hpp
+++ jp2_pc/Source/GUIApp/DialogTexturePack.hpp
@@ -62,7 +62,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogTexturePack)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 protected:
