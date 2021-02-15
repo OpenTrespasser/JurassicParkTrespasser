1c0366ee0b3666dbb2abf6a0f13d24d2af029d72
Michael <michael_kuerbis@web.de>
2021-02-15 21:32:19 +0100

    DialogScheduler.hpp: use override keyword

--- jp2_pc/Source/GUIApp/DialogScheduler.hpp
+++ jp2_pc/Source/GUIApp/DialogScheduler.hpp
@@ -23,7 +23,7 @@ public:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CDialogScheduler)
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
 // Implementation
