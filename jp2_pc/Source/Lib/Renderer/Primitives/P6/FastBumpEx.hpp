ec65f8429b40e2562d46c8ded8c4fa302e6760e1
Michael <michael_kuerbis@web.de>
2021-02-28 23:24:25 +0100

    P6/FastBumpEx.hpp:  make include guard specific to P6

--- jp2_pc/Source/Lib/Renderer/Primitives/P6/FastBumpEx.hpp
+++ jp2_pc/Source/Lib/Renderer/Primitives/P6/FastBumpEx.hpp
@@ -37,8 +37,8 @@
  * 
  **********************************************************************************************/
 
-#ifndef HEADER_LIB_RENDERER_PRIMITIVES_P5_FASTBUMPEX_HPP
-#define HEADER_LIB_RENDERER_PRIMITIVES_P5_FASTBUMPEX_HPP
+#ifndef HEADER_LIB_RENDERER_PRIMITIVES_P6_FASTBUMPEX_HPP
+#define HEADER_LIB_RENDERER_PRIMITIVES_P6_FASTBUMPEX_HPP
 
 // Make sure this file is being included from 'FastBump.hpp'.
 #ifndef HEADER_LIB_RENDERER_PRIMITIVES_FASTBUMP_HPP
