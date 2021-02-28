17fc4ee43c35b29d534f181e7fcc1d9af25b94ed
Michael <michael_kuerbis@web.de>
2021-02-28 23:55:23 +0100

    QuantizerTool/stdafx.h: add include guard

--- jp2_pc/Source/Tools/QuantizerTool/stdafx.h
+++ jp2_pc/Source/Tools/QuantizerTool/stdafx.h
@@ -3,6 +3,8 @@
 //      are changed infrequently
 //
 
+#pragma once
+
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
