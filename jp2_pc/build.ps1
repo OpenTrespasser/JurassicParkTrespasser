9b1400afff268f4f84c236581107d454390c4b05
Michael <michael_kuerbis@web.de>
2021-07-04 22:41:30 +0200

    Root/build.ps1: add basic build script that compiles OTP in all three configurations, runs the unittests and creates distributable package of OTP with the the demo data

---
+++ jp2_pc/build.ps1
@@ -0,0 +1,53 @@
+﻿$env:PATH = "$env:PATH;.\BuildTools\CMake\bin"
+$archivedir = New-Item -ItemType Directory -Force -Path .\Archive
+$builddir = New-Item -ItemType Directory -Force -Path .\Build\CI
+$basedir = Get-Location
+
+$configurations = @("Debug", "Release", "Final")
+
+function Generate-Solution(){
+    .\BuildTools\CMake\bin\cmake.exe -G "Visual Studio 16 2019" -A Win32 -S . -B $builddir -D USE_TRESPASSER_DIRECTORY=false
+}
+
+function Build-Solution([string] $config)
+{
+    Write-Host "Begin building configuration $config"
+
+    Push-Location $builddir
+    cmake --build . --config $config
+    ctest -C $config --verbose
+    $testresultdest = New-Item -ItemType Directory -Force -Path $archivedir\test\$config
+    foreach($resultfile in (Get-ChildItem -Path . -Filter *_TestResults.xml -File -Recurse)){
+        Copy-Item -Path $resultfile.FullName -Destination $testresultdest
+    }
+    Pop-Location
+}
+
+
+function Bundle-Demo([string] $config){
+    Push-Location $builddir
+    cpack -G ZIP -C $config -D CPACK_PACKAGE_FILE_NAME=OTP.$config --verbose
+    Move-Item -Path OTP.$config.zip -Destination $archivedir\OTP.$config.zip -Force
+    Pop-Location
+}
+
+function Bundle-Debuginfo(){
+    Push-Location $builddir
+    $tempdir = New-Item -ItemType Directory -Force -Path packaging\Debuginfo
+    foreach ($config in $configurations) {
+        Copy-Item -Recurse -Path .\cmake\trespass\$config\ -Filter *.pdb -Destination $tempdir -Force
+        Copy-Item -Recurse -Path .\cmake\trespass\$config\ -Filter *.map -Destination $tempdir -Force
+        Copy-Item -Recurse -Path .\cmake\trespass\$config\ -Filter *.exe -Destination $tempdir -Force
+    }
+    Compress-Archive -Path $tempdir\* -DestinationPath $archivedir\OTP-Debuginfo.zip -Force
+    Pop-Location
+}
+
+
+
+Generate-Solution
+foreach($config in $configurations){
+    Build-Solution $config
+    Bundle-Demo $config
+}
+Bundle-Debuginfo
